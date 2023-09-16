399d83c8e9037cb1544e9a6cf576503418596111
Ganesh Nalla <spankbummer@icloud.com>
2023-09-16 16:59:59 +0530

    initial commit

---
+++ raincheck/raincheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  raincheckApp.swift
+//  raincheck
+//
+//  Created by Ganesh Nalla on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct raincheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
